6737daf65e533778619ffc703f162c096382c60a
Lee Presswood <pressedlee123@gmail.com>
2014-05-20 18:47:01 -0500

    Revert "Starting to build my first intent"
    This reverts commit cff1a5e4fa7a1260a0a709ed54c50bb3b1e9a147.

--- GeoNode/src/com/leepresswood/geonode/MainActivity.java
+++ GeoNode/src/com/leepresswood/geonode/MainActivity.java
@@ -1,11 +1,8 @@
 package com.leepresswood.geonode;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	@Override
@@ -20,14 +17,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		
 		return true;
-	}
-	
-	public void sendMessage(View view)
-	{
-		//https://developer.android.com/training/basics/firstapp/starting-activity.html#BuildIntent
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
 	}
 }
