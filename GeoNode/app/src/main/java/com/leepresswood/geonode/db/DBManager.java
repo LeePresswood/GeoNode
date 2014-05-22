d4e293a3211b53006ef0068569a42ef95d36a05e
Lee Presswood <pressedlee123@gmail.com>
2014-05-22 02:56:50 -0500

    Added text for web service connection

--- GeoNode/app/src/main/java/com/leepresswood/geonode/db/DBManager.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/db/DBManager.java
@@ -1,6 +1,55 @@
 package com.leepresswood.geonode.db;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DBManager
 {
-	
+	//The DBManager will connect to a hosted DB
+	private static final String URL_STRING = "http://babbage.cs.missouri.edu/~lmp6yb/GeoNode/db.php";
+	private URL DB_URL;
+	private HttpURLConnection urlConnection;
+
+	public DBManager()
+	{//Attempt to set a URL
+		boolean URLflag = true;
+
+		try
+		{
+			DB_URL = new URL(URL_STRING);
+		} catch (MalformedURLException e)
+		{//Won't happen
+			e.printStackTrace();
+			URLflag = false;
+		}
+
+		if(URLflag)
+		{//URL set. Now make attempt to connect to web service.
+			boolean connectionFlag = true;
+			try
+			{
+				urlConnection = (HttpURLConnection) DB_URL.openConnection();
+			} catch (IOException e)
+			{//Site is down
+				e.printStackTrace();
+				connectionFlag = false;
+			}
+
+			//Connection is made. We can use the service now.
+			if(connectionFlag)
+				try
+				{
+					//InputStream in = new BufferedInputStream(urlConnection);
+					//readStream(in);
+				}
+				finally
+				{
+					urlConnection.disconnect();
+				}
+		}
+	}
 }
