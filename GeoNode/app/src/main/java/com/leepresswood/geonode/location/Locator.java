6793fb3fd233d7cba56b93973b500bf9bbcc1653
Lee Presswood <pressedlee123@gmail.com>
2014-05-24 14:04:17 -0500

    Removed unused imports

--- GeoNode/app/src/main/java/com/leepresswood/geonode/location/Locator.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/location/Locator.java
@@ -8,9 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 
 public class Locator
 {
