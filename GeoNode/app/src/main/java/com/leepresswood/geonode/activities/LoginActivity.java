a65584cc81a8bb7f826c1ec29b64c7f3ffe9fa7c
Lee Presswood <pressedlee123@gmail.com>
2014-05-25 01:46:15 -0500

    Added intent

--- GeoNode/app/src/main/java/com/leepresswood/geonode/activities/LoginActivity.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.leepresswood.geonode.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -43,6 +44,7 @@ public class LoginActivity extends ActionBarActivity {
 
     public void loginWithoutLogin(View view)
 	 {
+		 Intent i = new Intent();
 
     }
 }
