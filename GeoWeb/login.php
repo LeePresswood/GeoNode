5149d9ca4e5e545c669d6b892d2009a6d27bf1b9
Lee Presswood <pressedlee123@gmail.com>
2014-05-31 15:52:11 -0500

    Removed web services code for security reasons

--- GeoWeb/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-	/*The following is the called web service for the GeoNode login page.
-	  A user will attempt to log in from the app. The app will then call this service to interact with the database.
-	  Following a success, failure, or error, a response will be returned and parsed within the app.
-	*/
-?>
--- GeoWeb/map.php
+++
@@ -1,6 +0,0 @@
-<?php
-	/*The following is the called web service for the GeoNode map page.
-	  A user will attempt to interact with the location service within the app. The app will then call this service to interact with the database.
-	  Following a success, failure, or error, a response will be returned and parsed within the app.
-	*/
-?>
