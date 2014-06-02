273df1f1f94061b972f66fa9929c8f477ca17eb2
Lee Presswood <pressedlee123@gmail.com>
2014-06-02 02:37:20 -0500

    Trying a different approach to HTTPRequest

--- GeoNode/app/src/main/java/com/leepresswood/geonode/db/DBManager.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/db/DBManager.java
@@ -1,19 +1,15 @@
 package com.leepresswood.geonode.db;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class DBManager
 {
@@ -21,63 +17,52 @@ public class DBManager
 	private String URL_STRING;
 	private HttpURLConnection urlConnection = null;
 
-	private String connect(String q)
-	{//Submit the given query to a web service and return the response
+	private String connect(String q) throws IOException {//Submit the given query to a web service and return the response
         //Replaces spaces
         q = q.replace('_', ' ');
 
-	   HttpClient httpclient = new DefaultHttpClient();
-        HttpResponse response = null;
+        // Build the JSON object to pass parameters
+        JSONObject jsonObj = new JSONObject();
+        //jsonObj.put("username", username);
+        //jsonObj.put("data", dataValue);
 
-         try {
-            response = httpclient.execute(new HttpGet(URL_STRING));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }return "here";/*
-        if(response != null)
-        {
-            StatusLine statusLine = response.getStatusLine();
+        // Create the POST object and add the parameters
+        HttpPost httpPost = new HttpPost(URL_STRING);
+        StringEntity entity = new StringEntity(jsonObj.toString(), HTTP.UTF_8);
+        entity.setContentType("application/json");
+        httpPost.setEntity(entity);
 
-            if(statusLine.getStatusCode() == HttpStatus.SC_OK)
-            {
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                try {
-                    response.getEntity().writeTo(out);
-                    out.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(httpPost);
 
-                return out.toString();
-            }
-            else
-            {//Closes the connection with a failure
-                try {
-                    response.getEntity().getContent().close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return "GeoNodeError";*/
+        return "GeoNodeError";
 	}
 
 	public boolean query(String url, String q)
-	{//Pass in a query for Psql. Return success or failure.
+    {//Pass in a query for Psql. Return success or failure.
 		this.URL_STRING = url;
-		if(connect(q.replace(' ', '_')).equalsIgnoreCase("GeoNodeError"))
-			return false;
+        try {
+            if(connect(q.replace(' ', '_')).equalsIgnoreCase("GeoNodeError"))
+                return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
         //Connection successful
 		return true;
 	}
 
 	public String queryGetData(String url, String q)
-	{//Pass in Psql query. Return string of data returned
+    {//Pass in Psql query. Return string of data returned
 		this.URL_STRING = url;
-        return connect(q.replace(' ', '_'));
-        //return URL_STRING;
+        try {
+            return connect(q.replace(' ', '_'));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "GeoNodeError";
 	}
 
 	public static String htmlspecialchars(String s)
