7ef5db6d23c8b44a4cfc01eb602ec1c31a85aea1
Lee Presswood <pressedlee123@gmail.com>
2014-06-10 03:05:00 -0500

    Explain the AsyncTaskResultEvent

--- GeoNode/app/src/main/java/com/leepresswood/geonode/db/AsyncTaskResultEvent.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/db/AsyncTaskResultEvent.java
@@ -1,6 +1,16 @@
 package com.leepresswood.geonode.db;
 
 public class AsyncTaskResultEvent
-{
-	
+{//AsyncTask sets this result. Grab the result from the activity.
+	private String result;
+
+	public AsyncTaskResultEvent(String result)
+	{
+		this.result = result;
+	}
+
+	public String getResult()
+	{
+		return result;
+	}
 }
