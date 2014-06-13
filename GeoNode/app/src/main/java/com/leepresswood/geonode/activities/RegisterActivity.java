216d03c6317eb1359e3b0e93acd0c2e2a0c9ece0
Lee Presswood <pressedlee123@gmail.com>
2014-06-13 00:08:28 -0500

    Imported logic from login

--- GeoNode/app/src/main/java/com/leepresswood/geonode/activities/RegisterActivity.java
+++ GeoNode/app/src/main/java/com/leepresswood/geonode/activities/RegisterActivity.java
@@ -1,19 +1,55 @@
 package com.leepresswood.geonode.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.leepresswood.geonode.R;
+import com.leepresswood.geonode.db.ChangeListener;
+import com.leepresswood.geonode.db.DBManager;
 
 public class RegisterActivity extends Activity
 {
+	private DBManager dbm;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+
+		//This listener fires when the result is ready
+		final RegisterActivity holder = this;
+		ChangeListener listener = new ChangeListener()
+		{
+			@Override
+			public void stateChanged()
+			{
+				//If the response is anything but 1, we have not logged in properly.
+				if(dbm.resultString == "1")
+				{//Logged in successfully. Go to home page for that person.
+					Intent i = new Intent(holder, MapActivity.class);
+
+					//Pass in the username for the session
+					i.putExtra("username", ((EditText) holder.findViewById(R.id.textfield_username)).getText().toString());
+					startActivity(i);
+				}
+				else
+					//Improper login. Ask again
+					Toast.makeText(holder.getApplicationContext(), "Error: Incorrect username or password.", Toast.LENGTH_SHORT).show();
+			}
+		};
+
+		dbm = new DBManager(this.getApplicationContext(), listener);
+
+		//Display toast if we're connected
+		Toast.makeText(this.getApplicationContext(), dbm.isConnected(this) ? "You are connected." : "You are not connected.", Toast.LENGTH_LONG).show();
+
 	}
 
 
@@ -36,4 +72,8 @@ public class RegisterActivity extends Activity
 	}
 
 
+	public void register(View view)
+	{//Submit login details to the server for registration
+
+	}
 }
